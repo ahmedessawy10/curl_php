4c7b3a107344ac8be019866999ac03e36e23fab3
ahmedessawy10 <elessawy238@gmail.com>
2024-06-17 22:07:41 +0300

    curl google search with search value

--- index.php
+++ index.php
@@ -2,27 +2,25 @@
 
 # curl from google
 # https://developers.google.com/custom-search/v1/using_rest
+// $curl =curl_init();
+// curl_setopt($curl,CURLOPT_URL,"https://www.google.com");
+// curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
+// $result=curl_exec($curl);
+// curl_close($curl);
+// print_r($result);
+
+# curl from google with search 
 $curl =curl_init();
-curl_setopt($curl,CURLOPT_URL,"https://www.google.com");
+$search_label="iphone 10 pro max";
+$search_label=implode("+", explode(" ", $search_label));
+
+curl_setopt($curl,CURLOPT_URL,"https://www.google.com/search?q=$search_label");
 curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
+curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
+
 $result=curl_exec($curl);
 curl_close($curl);
-print_r($result);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+echo($result);
 
 
 ?>
